eb3bc3bc9683113d01905653d8be8d1d3f73c23f
Mina <mina@mina.pc>
2021-02-07 14:31:02 +0330

    setPriorityTest completed successfully

--- setPriorityTest.c
+++ setPriorityTest.c
@@ -9,8 +9,9 @@ int main (int argc, char *argv[]){
     int input=atoi(argv[1]);
    // int ppid=getpid();
    // printf(1,"im root %d \n",getpid());
+    int priority=getPriority();
     int res=setPriority(input);
-    printf(1,"pid is %d priority changed to = %d  \n",getpid(),res);
+    printf(1,"previous priority was %d priority changed to = %d  \n",priority,res);
     
     exit();
 }
